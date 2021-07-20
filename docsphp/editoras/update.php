348db0a8c80efde50e0c9c9f82a16d5a634728ce
glauciapamponet <glauciapamponet@gmail.com>
2021-07-20 14:30:49 -0300

    VCBD dia 2

---
+++ docsphp/editoras/update.php
@@ -0,0 +1,21 @@
+<?php
+$connect = mysqli_connect("localhost", "teste", "mysql", "vcb");
+
+// echo "$_POST";
+
+if(isset($_POST["id"]))
+{
+ $value = mysqli_real_escape_string($connect, $_POST["value"]);
+ if($_POST["column_name"] == 'nomeedit'){
+   $query = "UPDATE editoras SET ".$_POST["column_name"]."='".$value."' WHERE ideditoras = '".$_POST["id"]."'";
+ }
+ else{
+   $query = "UPDATE represedit SET ".$_POST["column_name"]."='".$value."' WHERE idrepresedit = '".$_POST["id"]."'";
+ }
+
+ if(mysqli_query($connect, $query))
+ {
+  echo 'Data Updated';
+ }
+}
+?>

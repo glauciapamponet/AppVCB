0f01be78b83e19e01e57c22771b18a0b629aa97b
glauciapamponet <glauciapamponet@gmail.com>
2021-07-23 14:43:53 -0300

    aaaaaa

--- pages/cadVen.php
+++ pages/cadVen.php
@@ -229,29 +229,21 @@
                               </div>
                               <div class="table-responsive">
                             		<button id="table2-new-row-button" class="btn  btn-primary btn-sm m-l-15 waves-effect"><i class="material-icons">add_circle_outline</i></button>
-                        		  	<table id="table2" class="table table-bordered table-striped table-hover  dataTable" >
+                        		  	<table id="crud_table" class="table table-bordered table-striped table-hover" >
                         			  	<thead class="thead-dark">
                         				    <tr>
-                        				      <th scope="col">#</th>
+                        				      <!-- <th scope="col">#</th> -->
                         				      <th scope="col">id Livro</th>
                                       <th scope="col">Quant</th>
+                                      <th></th>
                         				    </tr>
                         			  	</thead>
                         			  	<tbody>
                         				    <tr>
-                        				      <th scope="row">1</th>
-                        				      <td>Mark</td>
-                                      <td>Mark</td>
-                        				    </tr>
-                        				    <tr>
-                        				      <th scope="row">2</th>
-                        				      <td>Jacob</td>
-                                      <td>Jacob</td>
-                        				    </tr>
-                        				    <tr>
-                        				      <th scope="row">3</th>
-                        				      <td>Larry</td>
-                                      <td>Larry</td>
+                                      <td contenteditable="true" class="item_name"></td>
+                                      <td contenteditable="true" class="item_code"></td>
+                                      <td contenteditable="true" class="item_desc"></td>
+                                      <td></td>
                         				    </tr>
                         			  	</tbody>
                         			</table>
@@ -276,6 +268,75 @@
             <!-- #END# Multi Column -->
         </div>
     </section>
+
+    <script>
+$(document).ready(function(){
+ var count = 1;
+ $('#add').click(function(){
+  count = count + 1;
+  var html_code = "<tr id='row"+count+"'>";
+   html_code += "<td contenteditable='true' class='item_name'></td>";
+   html_code += "<td contenteditable='true' class='item_code'></td>";
+   html_code += "<td contenteditable='true' class='item_desc'></td>";
+   html_code += "<td contenteditable='true' class='item_price' ></td>";
+   html_code += "<td><button type='button' name='remove' data-row='row"+count+"' class='btn btn-danger btn-xs remove'>-</button></td>";
+   html_code += "</tr>";
+   $('#crud_table').append(html_code);
+ });
+
+ $(document).on('click', '.remove', function(){
+  var delete_row = $(this).data("row");
+  $('#' + delete_row).remove();
+ });
+
+ $('#save').click(function(){
+  var item_name = [];
+  var item_code = [];
+  var item_desc = [];
+  var item_price = [];
+  $('.item_name').each(function(){
+   item_name.push($(this).text());
+  });
+  $('.item_code').each(function(){
+   item_code.push($(this).text());
+  });
+  $('.item_desc').each(function(){
+   item_desc.push($(this).text());
+  });
+  $('.item_price').each(function(){
+   item_price.push($(this).text());
+  });
+  $.ajax({
+   url:"insert.php",
+   method:"POST",
+   data:{item_name:item_name, item_code:item_code, item_desc:item_desc, item_price:item_price},
+   success:function(data){
+    alert(data);
+    $("td[contentEditable='true']").text("");
+    for(var i=2; i<= count; i++)
+    {
+     $('tr#'+i+'').remove();
+    }
+    fetch_item_data();
+   }
+  });
+ });
+
+ function fetch_item_data()
+ {
+  $.ajax({
+   url:"fetch.php",
+   method:"POST",
+   success:function(data)
+   {
+    $('#inserted_item_data').html(data);
+   }
+  })
+ }
+ fetch_item_data();
+
+});
+</script>
 
     <!-- Jquery Core Js -->
     <script src="../plugins/jquery/jquery.min.js"></script>
